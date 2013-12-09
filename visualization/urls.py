2d49792f8a025d7e256cf349c238bf060b59b3b6
Serguei Balanovich <thesergsb@gmail.com>
2013-12-08 17:37:54 -0800

    splash page done

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url 
+
 from visualization import views
 
 urlpatterns = patterns('',
--- visualization/views.py
+++ visualization/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+	template = loader.get_template('visualization/index.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
 
 # Create your views here.
