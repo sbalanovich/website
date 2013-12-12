3675e1a703a7ce9d9f325640fe3bd805acccb9c0
Serguei Balanovich <thesergsb@gmail.com>
2013-12-12 09:36:06 -0800

    Added pages, fixed syntax error

--- visualization/urls.py
+++ visualization/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from visualization import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.collect, name='collect'),
-    url(r'^$', views.build, name='build'),
-    url(r'^$', views.visualize, name='visualize'),
-    url(r'^$', views.findings, name='findings'),
-    url(r'^$', views.about, name='about'),
+    url(r'^$', views.index),
+    url(r'collect^$', views.collect),
+    url(r'build^$', views.build),
+    url(r'visualize^$', views.visualize),
+    url(r'findings^$', views.findings),
+    url(r'about^$', views.about),
     (r'^test_results/$', views.test_ajax),
 )
